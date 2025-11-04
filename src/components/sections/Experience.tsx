e605076fb87eecbd4f0f035a4ba2ecfa889b5bc3
walkerhsu <walkerhsu0808@gmail.com>
2025-11-04 14:07:24 +0800

    Use grid to display experience sections

--- src/components/sections/Experience.tsx
+++ src/components/sections/Experience.tsx
@@ -6,9 +6,9 @@ import {
   Chip,
   Stack,
   Button,
+  Grid,
 } from "@mui/material";
 import WorkHistoryIcon from "@mui/icons-material/WorkHistory";
-import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import { Link } from "react-router-dom";
 import { experiences } from "../../data/experiences";
 import { renderJobTitle, renderTime } from "../utils/experienceUtils";
@@ -34,65 +34,87 @@ export default function Experience() {
         </Typography>
       </Box>
 
-      <Stack spacing={3}>
+      <Grid container spacing={2} sx={{ py: 1 }}>
         {experiences.map((exp, index) => (
-          <Link
-            to={`/experience/${index}`}
-            key={index}
-            style={{ textDecoration: "none" }}
-          >
-            <Card variant="outlined">
-              <CardContent>
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "flex-start",
-                    mb: 2,
-                  }}
-                >
-                  <Box>
-                    <Typography variant="h6" component="h3">
-                      {exp.client}
-                    </Typography>
-                    {renderJobTitle(exp.title)}
-                    {renderTime(exp.period)}
+          <Grid item size={{ xs: 12, sm: 6 }} key={index}>
+            <Link
+              to={`/experience/${index}`}
+              key={index}
+              style={{ textDecoration: "none" }}
+            >
+              <Card variant="outlined" sx={{ height: "100%" }}>
+                <CardContent>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "flex-start",
+                      mb: 2,
+                    }}
+                  >
+                    <Box>
+                      <Typography variant="h6" component="h3">
+                        {exp.client}
+                      </Typography>
+                      {renderJobTitle(exp.title)}
+                      {renderTime(exp.period)}
+                    </Box>
                   </Box>
-                </Box>
 
-                <Typography variant="body2" color="text.secondary" paragraph>
-                  {exp.description}
-                </Typography>
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                    sx={{
+                      display: "-webkit-box",
+                      overflow: "hidden",
+                      WebkitBoxOrient: "vertical",
+                      WebkitLineClamp: 3,
+                      mb: 1,
+                    }}
+                  >
+                    {exp.description}
+                  </Typography>
 
-                <Stack direction="row" spacing={1} flexWrap="wrap" useFlexGap>
-                  {exp.tags.map((tag, tagIndex) => (
-                    <Chip key={tagIndex} label={tag} size="small" />
-                  ))}
-                </Stack>
-
-                {exp.links && exp.links.length > 0 && (
-                  <Stack direction="row" spacing={1} sx={{ mt: 2 }}>
-                    {exp.links.map((link, linkIndex) => (
-                      <Button
-                        key={linkIndex}
-                        variant="outlined"
-                        href={link.url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        startIcon={<link.icon size={16} />}
-                        sx={{ textTransform: "none" }}
-                        onClick={(e) => e.stopPropagation()}
-                      >
-                        {link.label}
-                      </Button>
+                  <Stack
+                    direction="row"
+                    spacing={1}
+                    sx={{
+                      display: "-webkit-box",
+                      overflow: "hidden",
+                      WebkitBoxOrient: "vertical",
+                      WebkitLineClamp: 1,
+                      mb: 1,
+                    }}
+                  >
+                    {exp.tags.map((tag, tagIndex) => (
+                      <Chip key={tagIndex} label={tag} size="small" />
                     ))}
                   </Stack>
-                )}
-              </CardContent>
-            </Card>
-          </Link>
+
+                  {exp.links && exp.links.length > 0 && (
+                    <Stack direction="row" spacing={1} sx={{ mt: 2 }}>
+                      {exp.links.map((link, linkIndex) => (
+                        <Button
+                          key={linkIndex}
+                          variant="outlined"
+                          href={link.url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          startIcon={<link.icon size={16} />}
+                          sx={{ textTransform: "none" }}
+                          onClick={(e) => e.stopPropagation()}
+                        >
+                          {link.label}
+                        </Button>
+                      ))}
+                    </Stack>
+                  )}
+                </CardContent>
+              </Card>
+            </Link>
+          </Grid>
         ))}
-      </Stack>
+      </Grid>
     </Box>
   );
 }
